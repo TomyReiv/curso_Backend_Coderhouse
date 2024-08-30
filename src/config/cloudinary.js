75c4617e28e6d13121541d4322bbb7d26d722e66
Tomas <ravetomas@gmail.com>
2024-08-30 21:55:35 +0200

    arreglos al cloudinarey

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,10 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary'
-import { CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_NAME } from './config.js'
+import { config } from './config.js'
 
 cloudinary.config({
-	cloud_name: CLOUD_NAME,
-	api_key: CLOUD_API_KEY,
-	api_secret: CLOUD_API_SECRET
+	cloud_name: config.CLOUD_NAME,
+	api_key: config.CLOUD_API_KEY,
+	api_secret: config.CLOUD_API_SECRET
 })
 
 export default cloudinary
